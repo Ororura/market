02c57248a2b7dcb2bacd62105659459635e90216
Ororura <hitmanlol890@gmail.com>
2024-03-31 10:11:44 +0300

    init commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Sidebar } from "../widgets/sidebar";
+import { Main } from "../pages/main/Main.tsx";
+
+const App = () => {
+  return (
+    <Sidebar>
+      <Main></Main>
+    </Sidebar>
+  );
+};
+
+export default App;
---
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from "react";
+import styles from "./Sidebar.module.css";
+interface ISidebarProps {
+  children: ReactNode;
+}
+
+export const Sidebar: FC<ISidebarProps> = ({ children }) => {
+  return (
+    <>
+      <div className={styles.sidebar}>
+        <div className={styles.iconsSidebar}>
+          <img id={styles["logo"]} src={"./assets/photo/img.png"} width="60" height="64" alt=" " />
+        </div>
+        {/*<Cart></Cart>*/}
+      </div>
+      {children}
+    </>
+  );
+};
